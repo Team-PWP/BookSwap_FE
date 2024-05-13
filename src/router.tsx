0b1ddd734c5beb8b47aafac848d21d08cc953f9a
Dobbymin <gmin0701@knu.ac.kr>
2024-05-14 02:30:05 +0900

    feat: 라우터 설정

---
+++ src/router.tsx
@@ -0,0 +1,43 @@
+import { createBrowserRouter } from 'react-router-dom';
+import MainPage from './pages/MainPage'; 
+import LoginPage from './pages/login/LoginPage'
+import ChattingPage from './pages/chatting/ChattingPage';
+import UserProductPage from './pages/product/UserProductPage';
+import MyProductPage from './pages/product/MyProductPage';
+import SellPage from './pages/sell/SellPage';
+import DetailPage from './pages/detail/DetailPage';
+
+export const router = createBrowserRouter([
+    {
+        path: '/',
+        element: <MainPage />,
+    },
+    {
+        path: '/login',
+        element: <LoginPage />,
+    },
+    {
+        path: '/chatting',
+        element: <ChattingPage />,
+    },
+    {
+        path: '/product',
+        children: [
+            {   path: '/:user',
+                element: <UserProductPage />
+            },
+            {
+                path: '/my',
+                element: <MyProductPage />
+            },
+        ]
+    },
+    {
+        path: '/sell',
+        element: <SellPage />,
+    },
+    {
+        path: '/detail',
+        element: <DetailPage />,
+    },
+]);
