4ceaf3cc26bed6c521efb9778906ad467bcdf613
Dobbymin <gmin0701@knu.ac.kr>
2024-05-15 02:04:13 +0900

    feat: 환경변수 상수화

--- src/constants/URI.ts
+++ src/constants/URI.ts
@@ -1,1 +1,4 @@
 export const BASE_URI = import.meta.env.VITE_APP_BASE_URL;
+
+export const REST_API_KEY = import.meta.env.VITE_APP_REST_API_KEY;
+export const REDIRECT_URI = import.meta.env.VITE_APP_REDIRECT_URI;
