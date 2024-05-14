39bc4ae343f08c96fda3e22ab0abec36e9915689
Dobbymin <gmin0701@knu.ac.kr>
2024-05-15 02:04:49 +0900

    feat: Auth URL 상수화

---
+++ src/apis/OAuth.ts
@@ -0,0 +1,3 @@
+import { REDIRECT_URI, REST_API_KEY } from '@/constants/URI';
+
+export const KAKAO_AUTH_URL = `https://kauth.kakao.com/oauth/authorize?response_type=code&client_id=${REST_API_KEY}&redirect_uri=${REDIRECT_URI}`;
