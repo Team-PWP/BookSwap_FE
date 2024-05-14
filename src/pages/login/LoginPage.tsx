aae2c057a88774f27594445b9cef21885d617db2
Dobbymin <gmin0701@knu.ac.kr>
2024-05-15 02:06:10 +0900

    feat: LoginPage 구현

--- src/pages/login/LoginPage.tsx
+++ src/pages/login/LoginPage.tsx
@@ -1,55 +1,120 @@
-import Logo from '@assets/kakao.svg';
-
+import { KAKAO_AUTH_URL } from '@/apis/OAuth';
+import Logo from '@/assets/kakao.svg';
 import styled from '@emotion/styled';
 
 const LoginPage = () => {
-  // const GoogleLoginUrl = `
-  //   https://accounts.google.com/o/oauth2/v2/auth?response_type=code&client_id=${GOOGLE_AUTH_CLIENT_ID}&redirect_uri=${GOOGLE_AUTH_REDIRECT_URI}&scope=https://www.googleapis.com/auth/userinfo.profile https://www.googleapis.com/auth/userinfo.email
-  // `;
+  const HandlerKakaoLogin = () => {
+    window.location.href = KAKAO_AUTH_URL;
+    alert('카카오 로그인');
+  };
+
   return (
     <>
-      <LoginLayout>
-        <LoginSection>
-          <h1>Swap으로 중고거래 시작하기</h1>
-          <div>간편하게 가입하고 상품을 확인하세요</div>
-          <KaKaoLoginButton>
-            <KaKaoLoginLogo src={Logo} alt='kakao-symbol' />
-            <div>카카오 로그인</div>
-          </KaKaoLoginButton>
-        </LoginSection>
-      </LoginLayout>
+      <PCLayout>
+        <ChattingLayout></ChattingLayout>
+        <LoginLayout>
+          <PageName>로그인</PageName>
+          <LoginSection>
+            <h1>Swap으로 중고거래 시작하기</h1>
+            <div>간편하게 가입하고 상품을 확인하세요</div>
+            <ButtonSection>
+              <KaKaoLoginButton onClick={HandlerKakaoLogin}>
+                <KaKaoLoginLogo src={Logo} alt='kakao-symbol' />
+                <LoginText>카카오 로그인</LoginText>
+              </KaKaoLoginButton>
+            </ButtonSection>
+          </LoginSection>
+        </LoginLayout>
+        <BannerLayout></BannerLayout>
+      </PCLayout>
     </>
   );
 };
 
 export default LoginPage;
 
-const LoginLayout = styled.div`
-  text-align: center;
-  flex-direction: center;
+const PCLayout = styled.div`
+  display: flex;
+  justify-content: center;
+  align-items: center;
+  flex: 1 0 0;
+  align-self: stretch;
 `;
 
-const LoginSection = styled.section`
+const ChattingLayout = styled.div`
+  display: flex;
+  width: 18.75rem;
+  padding: 6.25rem 1.25rem 25rem 1.25rem;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+  gap: 0.625rem;
+  align-self: stretch;
+`;
+
+const BannerLayout = styled.div`
+  width: 18.75rem;
+  align-self: stretch;
+`;
+
+const PageName = styled.div`
+  display: flex;
+  font-size: 1.5rem;
+  font-weight: bold;
+  padding: 0.9375rem 0rem;
+  flex-direction: column;
+  justify-content: center;
+  align-items: flex-start;
+  align-self: stretch;
+`;
+
+const LoginLayout = styled.div`
+  display: flex;
+  /* text-align: start; */
+  flex-direction: column;
+  /* justify-content: center; */
+  align-items: start;
+  /* height: 70vh; */
+`;
+
+const LoginSection = styled.div`
   text-align: center;
   background-color: #f7f7f7;
   padding: 5rem 5rem;
+  border-radius: 0.75rem;
+`;
+const ButtonSection = styled.div`
+  display: flex;
+
+  text-align: center;
+  flex-direction: center;
+  margin-top: 2rem;
+  justify-content: center;
 `;
 
 const KaKaoLoginButton = styled.div`
   display: flex;
-  text-align: start;
-  flex-direction: center;
-  margin-top: 1rem;
+  text-align: center;
   background-color: #fee500;
-  padding: 1rem 0.5rem;
-  /* width: 20rem; */
-  /* height: 5.625rem; */
-  border-radius: 0.75rem;
+  width: 15rem;
+  height: 2.5rem;
+  border-radius: 0.5rem;
+  align-items: center;
 `;
 
 const KaKaoLoginLogo = styled.img`
-  width: 2rem;
-  height: 1.5rem;
-  margin-right: 4rem;
-  margin-left: 0.5rem;
+  width: 1.5rem;
+  text-align: center;
+  flex-direction: center;
+
+  margin-right: 2rem;
+  margin-left: 1rem;
+`;
+
+const LoginText = styled.div`
+  display: flex;
+  text-align: center;
+  flex-direction: center;
+  /* margin-top: 0.5rem; */
+  margin-left: 1rem;
 `;
