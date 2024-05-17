95e38101e60fa3be51a9651c648e72e896f5fc9d
Dobbymin <gmin0701@knu.ac.kr>
2024-05-17 10:02:40 +0900

    feat: redirect page 구현

--- src/pages/login/RedirectPage.tsx
+++ src/pages/login/RedirectPage.tsx
@@ -1,40 +1,18 @@
-import { useEffect, useState } from 'react';
-import { useNavigate } from 'react-router-dom';
+import { BASE_URI } from '@/constants/URI.ts';
+import { useOauth2Redirect } from '@/hooks/useOauth2Redirect.ts';
 
-import axios from 'axios';
-
-import { BASE_URI, REDIRECT_URI, REST_API_KEY } from '@/constants/URI';
-
-const RedirectPage = () => {
-  // const code = window.location.search;
-  // console.log(code);
-  const navigate = useNavigate();
+export const RedirectPage = () => {
   const CODE: string | null = new URLSearchParams(window.location.search).get(
     'code'
   );
   console.log(`${BASE_URI}/oauth2?code=${CODE}`);
-  useEffect(() => {
-    axios
-      .get(`${BASE_URI}/oauth2?code=${CODE}`, {
-        withCredentials: true,
-      })
-      .then((res) => {
-        let accessToken = res.headers.authorization;
-        console.log(accessToken);
 
-        //  받은 토큰 localStorage에 저장
-        localStorage.setItem('accessToken', accessToken);
-
-        // navigate('/');
-      });
-  }, []);
+  useOauth2Redirect(CODE);
 
   return (
     <>
-      <h2>로그인 성공!</h2>
+      <h2>로그인 진행중!</h2>
       <div>redirect page</div>
     </>
   );
 };
-
-export default RedirectPage;
