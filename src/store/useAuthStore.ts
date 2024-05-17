25cea4414d55d5aac598275fe5a22381103676d3
Dobbymin <gmin0701@knu.ac.kr>
2024-05-17 10:08:45 +0900

    feat: auth 상태관리 파일 생성

---
+++ src/store/useAuthStore.ts
@@ -0,0 +1,9 @@
+import create from 'zustand';
+
+interface AuthStore {
+  token?: string;
+}
+
+export const useAuthStore = create<AuthStore>((set) => ({
+  token: localStorage.getItem('accessToken'),
+}));
