b374d644e84c33efd16a4304f836cc001a9309c7
Dobbymin <gmin0701@knu.ac.kr>
2024-05-18 20:41:49 +0900

    feat: 내정보 페이지 interface 구현

--- src/pages/shop/type.ts
+++ src/pages/shop/type.ts
@@ -1,3 +1,25 @@
 export interface UserInfoLayoutProps {
   shopImg: string;
+  nickname?: string;
+  userInfo: UserInfo;
+}
+
+export interface UserInfo {
+  id: number;
+  nickname: string;
+  email: string;
+  userName: string;
+}
+
+export interface NicknameSectionProps {
+  nickname: string;
+}
+
+export interface EmailSectionProps {
+  email?: string;
+}
+
+export interface UserInfoContainerProps {
+  nickname?: string | undefined;
+  userInfo: UserInfo;
 }
