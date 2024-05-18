64141a5d5e4665b9a291d97fef7abff1ae815970
Dobbymin <gmin0701@knu.ac.kr>
2024-05-18 20:50:05 +0900

    feat: accessToken 전역변수 설정 및 메모리 저장 #issue-15 로 인해 사용 x

--- src/store/useAuthStore.ts
+++ src/store/useAuthStore.ts
@@ -1,9 +1,18 @@
-import create from 'zustand';
+import { create } from 'zustand';
 
-interface AuthStore {
-  token?: string;
+export interface AuthState {
+  accessToken: string;
+  setAccessToken: (token: string) => void;
+  logOut: () => void;
 }
+export const useStore = create<AuthState>((set) => ({
+  // access Token
+  accessToken: '',
 
-export const useAuthStore = create<AuthStore>((set) => ({
-  token: localStorage.getItem('accessToken'),
+  // access Token 저장
+  setAccessToken: (token: string) => set({ accessToken: token }),
+
+  // 로그아웃
+  logOut: () => set({ accessToken: '' }),
 }));
+
