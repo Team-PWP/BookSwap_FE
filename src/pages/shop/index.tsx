ee7782c0859c546f232208f050aa4b780843dcfa
Dobbymin <gmin0701@knu.ac.kr>
2024-05-18 20:41:17 +0900

    chore: kakao Login url 경로 변경

--- src/pages/shop/index.tsx
+++ src/pages/shop/index.tsx
@@ -1,13 +1,37 @@
+import { useState, useEffect } from 'react';
+
 import { TitleContainer } from './components/TitleContainer.tsx';
 import { UserInfoLayout } from './components/UserInfoLayout.tsx';
 import * as Styles from './styles';
+import { myInfo } from '@/apis/shop/shop.api.ts';
 import shopImg from '@/assets/Shop.png';
+import { UserInfo } from '@/pages/shop/type';
 
-export const ShopPage = () => {
+export const ShopPage: React.FC = () => {
+  const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
+
+  useEffect(() => {
+    const fetchMyInfo = async () => {
+      try {
+        const data = await myInfo();
+        setUserInfo(data);
+      } catch (error) {
+        console.error('유저 정보 받아오기 실패 :', error);
+      }
+    };
+
+    fetchMyInfo();
+  }, []);
+
+  if (!userInfo) {
+    return <div>loading...</div>;
+  }
+
+  // const { nickname, email } = userInfo;
   return (
     <Styles.ShopLayout>
       <TitleContainer />
-      <UserInfoLayout shopImg={shopImg} />
+      <UserInfoLayout shopImg={shopImg} userInfo={userInfo} />
     </Styles.ShopLayout>
   );
 };
