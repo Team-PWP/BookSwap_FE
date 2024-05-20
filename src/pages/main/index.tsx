9e107a760949f5d3bb0ce9f6766b82b6efe258e0
Dobbymin <gmin0701@knu.ac.kr>
2024-05-20 22:57:44 +0900

    chore: 파일명 변경

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,13 +1,13 @@
-import ChattingRoom from './components/ChattingRoom';
+import FreeMarket from './components/FreeMarket';
 import * as Styles from './styles';
 
 const MainPage = () => {
   return (
     <>
       <Styles.MainLayout>
-        <Styles.GlobalChattingLayout>
-          <ChattingRoom />
-        </Styles.GlobalChattingLayout>
+        <Styles.GlobalFreeMarketLayout>
+          <FreeMarket />
+        </Styles.GlobalFreeMarketLayout>
         <Styles.ContentsLayout>2</Styles.ContentsLayout>
         <Styles.BlankLayout>3</Styles.BlankLayout>
       </Styles.MainLayout>
