2b3b322ae6d9d4d45422fa62fb9f130bf408455e
Dobbymin <gmin0701@knu.ac.kr>
2024-05-20 17:54:25 +0900

    style: Global css 작성 및 적용

---
+++ src/styles/GlobalStyle.tsx
@@ -0,0 +1,18 @@
+import { css } from '@emotion/react';
+
+export const GlobalStyle = css`
+  body {
+    margin: 0;
+    padding: 0;
+  }
+
+  img {
+    display: block;
+  }
+
+  ul {
+    list-style: none;
+    padding: 0;
+    margin: 0;
+  }
+`;
