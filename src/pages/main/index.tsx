c0b66f31ea77f6e95d86de7ca255ca41144c6fc3
Dobbymin <gmin0701@knu.ac.kr>
2024-05-21 15:04:02 +0900

    style: PCLayout 구현 및 설정

--- src/pages/main/index.tsx
+++ src/pages/main/index.tsx
@@ -1,16 +1,17 @@
 import FreeMarket from './components/FreeMarket';
 import * as Styles from './styles';
+import { PCLayout } from '@/styles/PcLayout';
 
 const MainPage = () => {
   return (
     <>
-      <Styles.MainLayout>
+      <PCLayout>
         <Styles.GlobalFreeMarketLayout>
           <FreeMarket />
         </Styles.GlobalFreeMarketLayout>
         <Styles.ContentsLayout>2</Styles.ContentsLayout>
         <Styles.BlankLayout>3</Styles.BlankLayout>
-      </Styles.MainLayout>
+      </PCLayout>
     </>
   );
 };
--- src/pages/main/styles.ts
+++ src/pages/main/styles.ts
@@ -1,22 +1,5 @@
 import styled from '@emotion/styled';
 
-export const MainLayout = styled.div`
-  display: grid;
-  grid-template-columns: 1fr 2fr 1fr;
-  margin-top: 7.5rem;
-  width: 100%;
-  height: 100vh;
-
-  @media screen and (max-width: 1280px) {
-    /** 타블렛 가로, 노트북 */
-    grid-template-columns: 1fr 2fr 1fr;
-  }
-
-  @media screen and (min-width: 1281px) {
-    /** 데스크탑 */
-    grid-template-columns: 1fr 3fr 1fr;
-  }
-`;
 export const GlobalFreeMarketLayout = styled.div`
   text-align: center;
   align-items: center;
