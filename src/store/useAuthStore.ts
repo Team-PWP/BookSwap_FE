a9022561be909c02be214dbdd2bf9917b4eec4ff
Dongpil Jo <ehdvlf001@knu.ac.kr>
2024-05-22 18:29:42 +0900

    remove: 불필요한 파일 삭제

--- src/store/useAuthStore.ts
+++
@@ -1,9 +0,0 @@
-import create from 'zustand';
-
-interface AuthStore {
-  token?: string;
-}
-
-export const useAuthStore = create<AuthStore>((set) => ({
-  token: localStorage.getItem('accessToken'),
-}));
