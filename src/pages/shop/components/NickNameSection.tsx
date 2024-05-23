b190028cb2cd0de6ebd9e94af602b390de9ea847
Dobbymin <gmin0701@knu.ac.kr>
2024-05-23 19:29:13 +0900

    !HOTFIX: 사용하지 않는 코드 제거 및 수정

--- src/pages/shop/components/NickNameSection.tsx
+++ src/pages/shop/components/NickNameSection.tsx
@@ -1,26 +1,23 @@
-import { useEffect, useState } from 'react';
+import { useState } from 'react';
 
 import * as Styles from '../styles.ts';
-import { FixNickName, myInfo } from '@/apis/shop/shop.api';
+import { FixNickName } from '@/apis/shop/shop.api';
 import { NicknameSectionProps } from '@/pages/shop/type.ts';
-import { useUserInfoStore } from '@/store/useUserInfoStore.ts';
 
 export const NicknameSection: React.FC<NicknameSectionProps> = ({
   nickname,
 }) => {
   const [newNickname, setNewNickname] = useState<string>(nickname);
-  console.log('nicknameSection:', nickname);
 
-  // console.log('nicknameSection out:', nickname);
+  console.log(newNickname);
+
   const HandlerNickname = async () => {
     const reNickName: string | null = prompt('변경할 닉네임을 입력해주세요');
-    console.log(reNickName);
 
     if (reNickName) {
       try {
         const request = { nickname: reNickName };
         const data = await FixNickName(request);
-        // setNewNickName(data);
         console.log(data);
         alert(`닉네임을 ${reNickName}(으)로 변경하였습니다!`);
         setNewNickname(reNickName);
--- src/pages/shop/index.tsx
+++ src/pages/shop/index.tsx
@@ -1,60 +1,13 @@
-// import { useState, useEffect } from 'react';
-import { useEffect } from 'react';
-
 import { TitleContainer } from './components/TitleContainer.tsx';
 import { UserInfoLayout } from './components/UserInfoLayout.tsx';
 import * as Styles from './styles';
-import { myInfo } from '@/apis/shop/shop.api.ts';
 import shopImg from '@/assets/Shop.png';
-// import { UserInfo } from '@/pages/shop/type';
-import { useUserInfoStore } from '@/store/useUserInfoStore';
 
 export const ShopPage: React.FC = () => {
-  // const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
-  //
-  // const { userId, nickname, email, username } = useUserInfoStore<UserInfoState>(
-  //   (state) => state
-  // );
-  // const { setUserId, setNickname, setEmail, setUsername } = useUserInfoStore();
-  // //
-  // //
-  // useEffect(() => {
-  //   const fetchMyInfo = async () => {
-  //     try {
-  //       const data = await myInfo();
-  //
-  //       console.log(
-  //         'index.tsx: ',
-  //         data.userId,
-  //         data.nickname,
-  //         data.email,
-  //         data.username
-  //       );
-  //       setUserId(data.userId);
-  //       setNickname(data.nickname);
-  //       setEmail(data.email);
-  //       setUsername(data.username);
-  //     } catch (error) {
-  //       console.error('유저 정보 받아오기 실패 :', error);
-  //     }
-  //   };
-  //
-  //   fetchMyInfo();
-  // }, []);
-
-  // if (!userInfo) {
-  //   return <div>loading...</div>;
-  // }
-  // console.log(userInfo);
-
-  // const { nickname, email } = userInfo;
   return (
     <Styles.ShopLayout>
       <TitleContainer />
-      <UserInfoLayout
-        shopImg={shopImg}
-        // userInfo={userInfo}
-      />
+      <UserInfoLayout shopImg={shopImg} />
     </Styles.ShopLayout>
   );
 };
