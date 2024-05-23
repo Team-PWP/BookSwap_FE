1b92400f39140cc1454e30f983079e15ac2b64c8
Dobbymin <gmin0701@knu.ac.kr>
2024-05-23 17:44:47 +0900

    feat: 게시물 통신 코드 구현

---
+++ src/apis/article/article.response.ts
@@ -0,0 +1,9 @@
+export interface ArticleDetailCheckArticleResponse {
+  title: string;
+  content: string;
+  buyoutPrice: number;
+  minPrice: number;
+  bidStartAt: string;
+  bidEndAt: string;
+  imageUrls: string[];
+}
