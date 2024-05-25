2b07c0f235739f5f20e59e3572d79e04a100c4cf
Dobbymin <gmin0701@knu.ac.kr>
2024-05-25 23:13:37 +0900

    feat: 상품 판매 api 코드 구현

---
+++ src/store/useProductSellStore.ts
@@ -0,0 +1,39 @@
+import { create } from 'zustand';
+
+interface ProductSellState {
+  title: string;
+  setTitle: (title: string) => void;
+  content: string;
+  setContent: (content: string) => void;
+  buyoutPrice: number;
+  setBuyoutPrice: (buyoutPrice: number) => void;
+  minPrice: number;
+  setMinPrice: (minPrice: number) => void;
+  createdAt: string;
+  setCreatedAt: (createdAt: string) => void;
+  bidStartAt: string;
+  setBidStartAt: (bidStartAt: string) => void;
+  bidEndAt: string;
+  setBidEndAt: (bidEndAt: string) => void;
+  imageUrl: File[];
+  setImageUrl: (imageUrl: File[]) => void;
+}
+
+export const useProductSellStore = create<ProductSellState>((set) => ({
+  title: 'title',
+  content: 'nickname',
+  buyoutPrice: 0,
+  minPrice: 0,
+  createdAt: 'createdAt',
+  bidStartAt: 'bidStartAt',
+  bidEndAt: 'bidEndAt',
+  imageUrl: [],
+  setTitle: (title: string) => set({ title }),
+  setContent: (content: string) => set({ content }),
+  setBuyoutPrice: (buyoutPrice: number) => set({ buyoutPrice }),
+  setMinPrice: (minPrice: number) => set({ minPrice }),
+  setCreatedAt: (createdAt: string) => set({ createdAt }),
+  setBidStartAt: (bidStartAt: string) => set({ bidStartAt }),
+  setBidEndAt: (bidEndAt: string) => set({ bidEndAt }),
+  setImageUrl: (imageUrl: File[]) => set({ imageUrl }),
+}));
