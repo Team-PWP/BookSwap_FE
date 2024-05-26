dcd882b94aa4610a1dd338a8ec49b0a29653f9d6
김강민 <79429482+Dobbymin@users.noreply.github.com>
2024-05-26 14:49:46 +0900

    page 기능 수정 (#46)

--- src/pages/main/components/MainProduct.tsx
+++ src/pages/main/components/MainProduct.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useState } from 'react';
 import Pagination from 'react-js-pagination';
 
 import * as Styles from '../styles';
@@ -8,10 +9,17 @@ import { TitleContainer } from '@/styles/TitleStyle';
 
 const MainProduct = () => {
   const { page, setPage } = usePageStore();
+  const [activePage, setActivePage] = useState(page + 1); // 프론트엔드 페이지는 1부터 시작
+
+  useEffect(() => {
+    setActivePage(page + 1); // page가 변경될 때 activePage 업데이트
+  }, [page]);
 
   const handlePageChange = (page: number) => {
-    setPage(page);
+    setActivePage(page);
+    setPage(page - 1);
   };
+  console.log('현재페이지', page);
   return (
     <>
       <TitleContainer>오늘의 상품 추천</TitleContainer>
@@ -21,7 +29,7 @@ const MainProduct = () => {
       <Styles.PaginationContainer>
         <Styles.MyPageNation>
           <Pagination
-            activePage={page}
+            activePage={activePage}
             itemsCountPerPage={9}
             totalItemsCount={200}
             pageRangeDisplayed={5}
