f05f166ab754e21ede3b8118afa2a2efd1733bba
Dobbymin <gmin0701@knu.ac.kr>
2024-05-27 02:56:08 +0900

    feat: 입찰 기능 구현

--- src/pages/detail/components/ProductBid.tsx
+++ src/pages/detail/components/ProductBid.tsx
@@ -1,29 +1,22 @@
-import { useState, useEffect } from 'react';
-
+// import { useState, useEffect } from 'react';
 import * as Styles from '../styles';
 import { ProductBidProps } from '../type';
 import { useAuctionStore } from '@/store/useAuctionStore';
 
 const ProductBid: React.FC<ProductBidProps> = ({
   minPrice,
-  bidPrice,
+  //   bidPrice,
   bidTime,
 }) => {
   const { newBidPrice } = useAuctionStore();
-  const [bids, setBids] = useState<string[]>([]);
-  const [currentBid, setCurrentBid] = useState<number>(0);
+  //   const [bids, setBids] = useState<string[]>([]);
+  //   const [currentBid, setCurrentBid] = useState<number>(0);
 
-  useEffect(() => {
-    if (newBidPrice > 0) {
-      setCurrentBid(newBidPrice);
-    }
-  }, [newBidPrice]);
-
-  const handleBid = () => {
-    const newBid = `입찰가: ${currentBid}원`;
-    setBids([...bids, newBid]);
-    setCurrentBid(currentBid + 1);
-  };
+  //   useEffect(() => {
+  //     if (newBidPrice > 0) {
+  //       setCurrentBid(newBidPrice);
+  //     }
+  //   }, [newBidPrice]);
 
   return (
     <>
@@ -42,16 +35,17 @@ const ProductBid: React.FC<ProductBidProps> = ({
           <Styles.BidTableRow>
             <Styles.BidTableCell>최신 입찰 가격</Styles.BidTableCell>
             <Styles.BidTableCell>
-              {bidPrice.toLocaleString()}원
+              {/* {bidPrice.toLocaleString()}원 */}
+              {newBidPrice.toLocaleString()}원
             </Styles.BidTableCell>
           </Styles.BidTableRow>
         </Styles.BidTable>
         {/* <Styles.BidButton onClick={handleBid}>입찰하기</Styles.BidButton> */}
-        <Styles.BidStatusList>
+        {/* <Styles.BidStatusList>
           {bids.map((bid, index) => (
             <Styles.BidStatusItem key={index}>{bid}</Styles.BidStatusItem>
           ))}
-        </Styles.BidStatusList>
+        </Styles.BidStatusList> */}
       </Styles.ProductActionBidContainer>
     </>
   );
