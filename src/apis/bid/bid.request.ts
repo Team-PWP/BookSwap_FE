3a069bc41fd88b859560742a3de4a46e2bf8e077
Dobbymin <gmin0701@knu.ac.kr>
2024-05-29 16:12:34 +0900

    fix: 입찰정보 api 타입 수정

--- src/apis/bid/bid.request.ts
+++ src/apis/bid/bid.request.ts
@@ -4,4 +4,13 @@ export interface bidRequest {
   articles: number;
   price: number;
   bidId: number;
+  data: Bid[];
+}
+
+export interface Bid {
+  bidId: number;
+  articleId: number;
+  userNickname: string;
+  price: number;
+  bidTime: string;
 }
